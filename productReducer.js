12ed6bd08300a66086b1fff2774fe60d1ba07bc6
Ra-m0795 <ramofficial50@gmail.com>
2024-11-29 23:58:12 +0530

    Create productReducer.js

---
+++ productReducer.js
@@ -0,0 +1,16 @@
+// cartReducer.js
+const initialState = { items: [], totalCount: 0 };
+
+function cartReducer(state = initialState, action) {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      // Add item logic
+      break;
+    case 'REMOVE_FROM_CART':
+      // Remove item logic
+      break;
+    default:
+      return state;
+  }
+}
+export default cartReducer;
